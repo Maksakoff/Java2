53cd21b9d95e560019b4e924b3151553066c200e
Anton <maksakov.anton@yandex.ru>
2022-03-18 21:31:31 +0300

    run

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+        public static void main(String[] args) {
+
+            //Входные данные
+
+            int sum = 5000;
+            int part = 20;
+
+            //Логика
+
+            int bonus = sum / part;
+
+            System.out.println("При покупке билета стоимостью " + sum + " руб., Ваш бонус составит " + bonus + " бонусных миль.");
+        }
+    }
